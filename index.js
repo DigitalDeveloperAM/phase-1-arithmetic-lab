ffe8e74e488eb6f8c8be0db99eeab9c3101ee842
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-07-22 19:59:11 +0200

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * 10) + 2;
+
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+
+console.log(max);
